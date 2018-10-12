6506166544b4f411c102c89c9c3ab0acd2779f09
Zachery Gentry <zacherygentry@gmail.com>
2018-10-12 18:00:45 -0500

    simple firebase init on splash.js

--- src/scenes/Splash.js
+++ src/scenes/Splash.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Image, Button, } from 'react-native';
 import yeet from '../../images/yeet520.png';
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDlkT9b1WAM_AU_CH6bpCQavkhPtPH8sCs",
+  databaseURL: "https://yeet-54e58.firebaseio.com/"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 class Splash extends Component {
   static navigationOptions = {
     header: null
   };
 
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
@@ -16,15 +25,22 @@ class Splash extends Component {
   }
 
   render() {
+
+    function pushInt(num) {
+      firebase.database().ref('numbers/' + num).set({
+        count: num
+      });
+    }
+
     return (
       <View style={styles.container}>
         <Image source={yeet} style={styles.image}></Image>
         <Text style={styles.text}>
           YEET
         </Text>
-        <View style={styles.button}> 
+        <View style={styles.button}>
           <Button
-            onPress={() => this.props.navigation.navigate('home')}
+            onPress={() => {pushInt(5); this.props.navigation.navigate('home')}}
             title="Start yeetin'"
             color="#ffff"
             accessibilityLabel="Login button"
